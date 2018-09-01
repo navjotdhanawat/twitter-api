d0a35c12629a476bfa9f850144d37bdbad0adffd
Navjot <navjotdhanawat@gmail.com>
2018-09-02 01:31:34 +0530

    Added readme

---
+++ config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    consumer_key: 'czRNRGPNnbv9OoXEChfPIVrt8',
+    consumer_secret: 'z8dzdWixzX3ejC1KHnfAGOGZcqAEzvvqdM3EZp6yleCyIspNy0',
+    access_token: '197011746-wn2e0x7zc0uLO3tBcnZfLv03MJOs4a7cDtN1xl5G',
+    access_token_secret: 'VLeUBOGB26PmRqmR4cu8b7MS2Fh2zBYpxzCfibRTASk28',
+    timeout_ms: 60 * 1000,  // optional HTTP request timeout to apply to all requests.
+    strictSSL: true,
+}
--- index.js
+++ index.js
@@ -1,15 +1,16 @@
 var Twit = require('twit')
+var config = require('./config')
 
 var T = new Twit({
-    consumer_key: '',
-    consumer_secret: '',
-    access_token: '',
-    access_token_secret: '',
-    timeout_ms: 60*1000,  // optional HTTP request timeout to apply to all request.
-    strictSSL: true    
+    consumer_key: config.consumer_key,
+    consumer_secret: config.consumer_secret,
+    access_token: config.access_token,
+    access_token_secret: config.access_token_secret,
+    timeout_ms: config.timeout_ms,
+    strictSSL: config.strictSSL
 });
 
-T.get('search/tweets', { q: 'youtu.be #nowplaying', count: 500 }, function(err, data, response) {
+T.get('search/tweets', { q: 'youtu.be #nowplaying', count: 500 }, function (err, data, response) {
     console.log(data)
     // T.get('search/tweets', { q: '#nowplaying', count: 5, last_id: '1035966689534455800' }, function(err, data, response) {
     //     console.log(data)
